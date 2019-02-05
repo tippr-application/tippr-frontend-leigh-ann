8b8dc39d5dfd4889eaca1add81803613f8be8875
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-04 19:55:46 -0500

    Add Navigation to HomeView

--- eztip/src/components/views/HomeView/HomeView.js
+++ eztip/src/components/views/HomeView/HomeView.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router';
 
 import { GuestView } from '../GuestView';
 import { EmployeeView } from '../EmployeeView';
+import { Navigation } from '../../presentational/Navigation';
 
 import { connect } from 'react-redux';
 import { getEmployees } from '../../../store/actions';
@@ -20,6 +21,7 @@ class HomeView extends React.Component {
     render() {
         return (
             <div>
+                <Navigation />
                 <h1>Welcome</h1>
                 <Route exact path="/guest" component={GuestView} />
                 <Route exact path="/employee/:id" render={props => <EmployeeView {...props} users={this.props.users} />} />
